cc571a453379900e05ecb644bee4a18f51dcfa20
andrés camilo gómez orozco <andresc.gomez@upb.edu.co>
2022-05-17 15:20:15 -0500

    Update personajes.blade.php

--- resources/views/personajes.blade.php
+++ resources/views/personajes.blade.php
@@ -128,53 +128,6 @@ Author URL: http://w3layouts.com
         </div>
     </section> 
 
-
-
-    <section class="w3l-contact-info-main py-5" id="contact">
-        <div class="container py-md-5 py-4">
-            <div class="title-main text-center mx-auto mb-4" style="max-width:700px;">
-                <h3 class="title-style">Contact Us</h3>
-                <p class="sub-title mt-2">Cum doctus civibus efficiantur in imperdiet deterruisset. Cras efficitur,
-                    metus gravida suscipit cursus, dui diam pre lorem id lectus.</p>
-            </div>
-            <div class="contact-w3pvt-form mt-5 pt-lg-4">
-                <form method="post" class="w3layouts-contact-fm" action="https://sendmail.w3layouts.com/submitForm">
-                    <div class="row main-cont-sec">
-                        <div class="col-md-6 left-cont-contact">
-                            <div class="form-group mb-3">
-                                <label for="w3lName">First Name</label>
-                                <input class="form-control" type="text" name="w3lName" id="w3lName" placeholder=""
-                                    required="">
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="w3lName">Last Name</label>
-                                <input class="form-control" type="text" name="w3lName" id="w3lName" placeholder=""
-                                    required="">
-                            </div>
-                            <div class="form-group mb-3">
-                                <label for="w3lSender">Email</label>
-                                <input class="form-control" type="email" name="w3lSender" id="w3lSender" placeholder=""
-                                    required="">
-                            </div>
-                        </div>
-                        <div class="col-md-6 right-cont-contact">
-                            <div class="form-group">
-                                <label for="w3lSubject">Write Message</label>
-                                <textarea class="form-control" name="w3lMessage" id="w3lMessage" placeholder=""
-                                    required=""></textarea>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group-2 mt-3 text-right">
-                        <button type="submit" class="btn btn-style">Send Now</button>
-                    </div>
-                </form>
-                <iframe class="map-w3layouts"
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6509162.831475898!2d-123.79818215689443!3d37.19305185672543!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x808fb9fe5f285e3d%3A0x8b5109a227086f55!2sCalifornia%2C+USA!5e0!3m2!1sen!2sin!4v1535716535953"
-                    allowfullscreen></iframe>
-            </div>
-        </div>
-    </section>
     <!-- //contact -->
 
     <!-- footer -->
