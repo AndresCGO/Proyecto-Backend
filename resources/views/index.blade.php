a651b31d22f9bb2cf3228a69053f9243f5894926
andrés camilo gómez orozco <andresc.gomez@upb.edu.co>
2022-05-17 14:31:53 -0500

    centralizamos menu

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,40 +39,7 @@ Author URL: http://w3layouts.com
                     <span class="navbar-toggler-icon fa icon-close fa-times"></span>
                 </button>
 
-                <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-                    <ul class="navbar-nav ml-lg-auto">
-                        <li class="nav-item active">
-                            <a class="nav-link" href="index.html">Inicio <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.html">API Pokemon</a>
-                        </li>
-                        <li class="nav-item">
-                            <a id="armas" class="nav-link">Armas</a>
-                        </li>
-                        <li class="nav-item">
-                            <a id="personajes" class="nav-link" >Personajes</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="login.html">Ingresar</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="registro.html">Registrate</a>
-                        </li>
-                        <!-- search button -->
-                        <div class="search-right ml-lg-3">
-                            <form action="#search" method="GET" class="search-box position-relative">
-                                <div class="input-search">
-                                    <input type="search" placeholder="Enter Keyword" name="search" required="required"
-                                        autofocus="" class="search-popup">
-                                </div>
-                                <button type="submit" class="btn search-btn"><i class="fa fa-search"
-                                        aria-hidden="true"></i></button>
-                            </form>
-                        </div>
-                        <!-- //search button -->
-                    </ul>
-                </div>
+                @include('menu')
                 <!-- toggle switch for light and dark theme -->
                 <div class="cont-ser-position">
                     <nav class="navigation">
