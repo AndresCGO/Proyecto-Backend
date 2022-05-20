456803bcd9ddedc505aaced0fea4a44da5265119
sgez14 <samir.colpas@hotmail.com>
2022-05-19 19:10:53 -0500

    se agrego el paquete Elderflame al seeder

--- database/seeders/SkinSeeder.php
+++ database/seeders/SkinSeeder.php
@@ -14,6 +14,13 @@ class SkinSeeder extends Seeder
      */
     public function run()
     {
-        
+        $skin1 = new Skin();
+
+        $skin1->nombre_imagen="Elderflame.png";
+        $skin1->nombre_paquete="Elderflame";
+        $skin1->armas="Frenzy,Judge,Operator,Vandal,Meele";
+        $skin1->precio="10700 PV (~$100)";
+
+        $skin1->save();
     }
 }
